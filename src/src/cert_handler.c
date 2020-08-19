3cf442f7819515ebfe41f938236808f3836caed2
Joachim Müssig <uneid@student.kit.edu>
2020-08-19 12:20:43 +0000

    Implement and fix certificate handler

--- src/src/cert_handler.c
+++ src/src/cert_handler.c
@@ -1,25 +1,117 @@
 #include "exim.h"
+#include <stdio.h>
+#include <dirent.h>
 #include "cert_handler.h"
+#include <stdlib.h>
+#include <string.h>
 
 #define SIZE 26
+#define MAXSIZE 4194304
 
 Hash_element* hash_table[SIZE];
+
 
 /*
    currently only a test method.
    Idea: textfile or db with certificates in pem format -> load from file 
 */
 BOOL load_certificates(void) {
-   uschar *testAddress1 = US"joachim@testmail";
-   uschar *testAddress2 = US"joachim@mail1";
-   uschar *testAddress3 = US"joachim@mail2";
-   uschar *testcert1 = US"testcert1_joachim@testmail";
-   uschar *testcert2 = US"testcert2_joachim@mail1";
-   uschar *testcert3 = US"testcert3_joachim@mail2";
-   insert(testAddress1, testcert1);
-   insert(testAddress2, testcert2);
-   insert(testAddress3, testcert3);
+    DIR           *d;
+    struct dirent *dir;
+    //char cert[MAXSIZE];
+    //int size = 10;
+    //int nch = 0;
+    int c;
+    //char *cert = malloc(size);
+    char* path = "/home/joachim/eximNew/exim/userCerts";
+    char* filePath = "/home/joachim/eximNew/exim/userCerts/joachim@testmail";
+
+    d = opendir(path);
+    if (d) {
+      while ((dir = readdir(d)) != NULL) {
+	if (!strcmp(dir->d_name,"..") || !strcmp(dir->d_name,".")) continue;
+	uschar* address = dir->d_name;
+	//char* filePath = path;
+	//strcat(filePath, "/");
+	//strcat(filePath, address); 
+	printf("	try to open %s\n", filePath);
+    	FILE *file = fopen(filePath, "rb");
+	if(file) {
+	  debug_printf("	read file %s\n", address);
+	  //int count = fread(&cert, sizeof(char), 20, file);
+	  //if(cert == NULL) {
+		  //debug_printf("out of memory\n");
+	  //}
+
+	  char *cert = malloc(MAXSIZE);
+	  if (file) {
+		  if (cert) {
+			  size_t numread = fread(cert, sizeof *cert, MAXSIZE, file);
+
+			  // shrink if desired
+			  char *tmp = realloc(cert, numread+1);
+			  if (tmp) {
+				  tmp[numread] = '\0';
+				  cert = tmp;
+				  // Use buf with numread char
+			  }
+			  //free(buf);
+		  } else {
+			  free(cert);
+		  }
+
+		  //fclose(file);
+	  } else {
+		  free(cert);
+	  }
+
+
+
+
+
+		  //debug_printf("%s", cert);
+	  //while (fgets(cert, MAXSIZE, file) != NULL) {
+		  //debug_printf("%s", cert);
+		  //temp[strcspn(temp, "\n")] = 0;//remove newline
+            	  //strcat(temp, &fileBuffer[1]);
+	  if(cert) {
+	    debug_printf("\n	cert: %s\n", cert);
+	    insert(address, cert); 
+	  } else {
+            DEBUG(D_transport) {
+		    debug_printf("	cert is NULL");
+	    }
+	  }
+	  
+	} else {
+	    DEBUG(D_transport) {
+		    errno = 0;
+		    debug_printf("	Value of errno: %d\n", errno);
+		    debug_printf("	could not open file\n");
+	    }
+	}
+	fclose(file);
+      }
+      closedir(d);
+    } else {
+	    DEBUG(D_transport) {
+		    debug_printf("	certificate directory not found\n");
+		    debug_printf("	Value of errno: %d\n", errno);
+	    }
+    }
+	
+   //uschar *testAddress1 = US"joachim@testmail";
+   //uschar *testAddress2 = US"joachim@mail1";
+   //uschar *testAddress3 = US"joachim@mail2";
+   //uschar *testcert1 = US"testcert1_joachim@testmail";
+   //uschar *testcert2 = US"testcert2_joachim@mail1";
+   //uschar *testcert3 = US"testcert3_joachim@mail2";
+   //insert(testAddress1, testcert1);
+   //insert(testAddress2, testcert2);
+   //insert(testAddress3, testcert3);
 }
+
+
 
 /*
     search for an certificate given the mail address in receiver_address and save it at the address given in cert
@@ -102,8 +194,12 @@ Hash_element *search_hash_table_index(uschar *key) {
 void insert(uschar *key,uschar *data) {
 
    Hash_element *item = (Hash_element*) malloc(sizeof(Hash_element));
-   item->cert = data;  
-   item->address = key;
+   //item->cert = *data;  
+   //item->address = *key;
+   item->cert = malloc(strlen(data)+1);
+   strcpy(item->cert, data);
+   item->address = malloc(strlen(key)+1);
+   strcpy(item->address, key);
    item->next = NULL;
 
    //get the hash 
@@ -116,11 +212,13 @@ void insert(uschar *key,uschar *data) {
   	 
 
    if(element == NULL) {
+      debug_printf("	first element at index %d\n", hashIndex);
       hash_table[hashIndex] = item;
    } else {
+      debug_printf("	not first element! \n");
       while(element->next != NULL) {
 	DEBUG(D_transport) {
-      		debug_printf("    	 %s -> \n", element->address);
+      		debug_printf("ELEMENTADDRESS     	 %s -> \n", element->address);
    	}
          element = element->next;
 
--- src/src/cert_handler.h
+++ src/src/cert_handler.h
@@ -14,8 +14,8 @@ int hashNumber(int key);
 
 int hashString(uschar *key);
 
-uschar * search_value_in_list(Hash_element * element, uschar *key);
+uschar * search_value_in_list(Hash_element * element, uschar * key);
 
-Hash_element *search_hash_table_index(uschar *key);
+Hash_element *search_hash_table_index(uschar * key);
 
 void insert(uschar *key,uschar *data);
